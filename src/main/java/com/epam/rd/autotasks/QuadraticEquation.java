51d68a0ff72464a92940291b985b7764e3a44829
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-09-05 23:02:27 +0400

    QuadraticEquation: Commit N1

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -10,9 +10,24 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        findRoot(a, b, c);
     }
 
+    /*
+    Calculates the root of the equation, if there is such.
+     */
+    public static void findRoot(double a, double b, double c) {
+        double x1, x2;
+        double determinant = Math.pow(b, 2) - 4 * a * c;
+        if(determinant < 0) {
+            System.out.println("no roots");
+        } else {
+            x1 = (-b + sqrt(determinant))/(2*a);
+            x2 = (-b - sqrt(determinant))/(2*a);
+            System.out.println(x1);
+            if(determinant > 0) {
+                System.out.print(" " + x2);
+            }
+        }
+    }
 }
